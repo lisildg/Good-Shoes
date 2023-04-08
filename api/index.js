c8b44394c447b9e992a564b2d04a86ff917907ec
Franco Valdez <franco.martin.valdez@mi.unc.edu.ar>
2023-04-07 21:40:33 -0300

    configurando el back - aun sin sequelize

---
+++ api/index.js
@@ -0,0 +1,7 @@
+const app=require("./src/app")
+
+
+
+app.listen(3001,()=>{
+    console.log("app listen en port 3001")
+})
---
+++ api/src/routes/route-index.js
@@ -0,0 +1,12 @@
+const { Router } = require("express");
+
+
+
+
+
+
+const ruta_principal=Router()
+
+
+
+module.exports=ruta_principal
