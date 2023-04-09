97874b118df89477db911082f17dfafc06012bb4
Franco Valdez <franco.martin.valdez@mi.unc.edu.ar>
2023-04-09 16:18:19 -0300

    modelo : variante

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const app=require("./src/app")
 const sequelize=require("./src/database")
 const port = 3000
 const {models}=require("./src/models/Relations")
-console.log(models)
+console.log(Object.values(models))
 sequelize.sync({force:true}).then(()=>{
 
   app.listen(port, () => {
