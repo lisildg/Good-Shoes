3f94d548cfc2aaebd18e5b5dabaf844b8b713e3a
Lisi Gonzalez <lisigonzalez@outlook.com>
2023-04-12 23:45:20 -0300

    intalacion DaisyUi y cong tailwind

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  darkMode: 'class',
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "dark",
+  },
 }
 
