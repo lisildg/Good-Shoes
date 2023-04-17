e97648106a3fc5010eaf7315e41624d2fda8a771
Lisi Gonzalez <lisigonzalez@outlook.com>
2023-04-17 16:16:50 -0300

    cambios en navbar

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function NavBar(){
     return(
-        <div className="navbar bg-base-100">
+        <div className="navbar bg-blue-500">
   <div className="flex-1">
     <a className="btn btn-ghost normal-case text-xl">Good Shoes</a>
   </div>
