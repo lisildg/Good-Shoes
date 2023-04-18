9b311e731aa860d19e3cb4390b1b78c28e6552b0
Lisi Gonzalez <lisigonzalez@outlook.com>
2023-04-18 15:38:41 -0300

    implmentando autentificacion

--- api/src/auth/firebase.js
+++ api/src/auth/firebase.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
 import {getAuth } from 'firebase/auth';
+const admin =require('firebase-admin')
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -14,9 +15,16 @@ const firebaseConfig = {
   messagingSenderId: FIREBASE_SENDER_ID,
   appId: FIREBASE_APP_ID
 };
-
+const clavePrivada = require('./firebase.json')
 // Initialize Firebase
+admin.initializeApp({
+  credential: admin.credential.cert(clavePrivada),
+  databaseURL: 'firebase-adminsdk-9565h@good-shoes.iam.gserviceaccount.com'
+});
+firebaseConfig.initializeApp(firebaseConfig);
+
 const app = initializeApp(firebaseConfig);
 export const auth= getAuth();
-
 export default getFirestore(app)
+
+module.exports = {firebase, admin}
